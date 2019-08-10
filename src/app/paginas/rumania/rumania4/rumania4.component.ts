d4c7ef3c1163e83f2d0ef4c1390961bd8677a11e
MarcosS <sanzgonzalezmarcos@gmail.com>
2019-08-10 11:00:48 +0200

    añadido dia 3 rumania

--- src/app/paginas/rumania/rumania4/rumania4.component.ts
+++ src/app/paginas/rumania/rumania4/rumania4.component.ts
@@ -7,24 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Rumania4Component implements OnInit {
 
-  imagen1 = '../../../assets/imagenes/galeriaRumania/rum5.jpg';
-  imagen2 = '../../../assets/imagenes/galeriaRumania/rum6.jpg';
-  imagen3 = '../../../assets/imagenes/galeriaRumania/rum9.jpg';
-  imagen4 = '../../../assets/imagenes/galeriaRumania/rum8.jpg';
-  imagen5 = '../../../assets/imagenes/galeriaRumania/rum10.jpg';
-  imagen6 = '../../../assets/imagenes/galeriaRumania/rum11.jpg';
-  imagenModal1 = '../../../assets/imagenes/galeriaRumania/rum5.jpg';
-  imagenModal2 = '../../../assets/imagenes/galeriaRumania/rum6.jpg';
-  imagenModal3 = '../../../assets/imagenes/galeriaRumania/rum9.jpg';
-  imagenModal4 = '../../../assets/imagenes/galeriaRumania/rum8.jpg';
-  imagenModal5 = '../../../assets/imagenes/galeriaRumania/rum10.jpg';
-  imagenModal6 = '../../../assets/imagenes/galeriaRumania/rum11.jpg';
-  titulo1 = 'Monasterio de Curtea de Arges';
-  titulo2 = 'Monasterio de Curtea de Arges';
-  titulo3 = 'Interior Monasterio de Curtea de Arges';
-  titulo4 = 'Subida al Castillo de Vald Tepes';
-  titulo5 = 'Subida al Castillo de Vald Tepes';
-  titulo6 = 'KSubida al Castillo de Vald Tepes';
+  imagen1 = '../../../assets/imagenes/galeriaRumania/rum-4-10.jpg';
+  imagen2 = '../../../assets/imagenes/galeriaRumania/rum-4-7.jpg';
+  imagen3 = '../../../assets/imagenes/galeriaRumania/rum-4-8.jpg';
+  imagen4 = '../../../assets/imagenes/galeriaRumania/rum-4-9.jpg';
+  imagen5 = '../../../assets/imagenes/galeriaRumania/rum-4-5.jpg';
+  imagen6 = '../../../assets/imagenes/galeriaRumania/rum-4-1.jpg';
+  imagenModal1 = '../../../assets/imagenes/galeriaRumania/rum-4-10.jpg';
+  imagenModal2 = '../../../assets/imagenes/galeriaRumania/rum-4-7.jpg';
+  imagenModal3 = '../../../assets/imagenes/galeriaRumania/rum-4-8.jpg';
+  imagenModal4 = '../../../assets/imagenes/galeriaRumania/rum-4-9.jpg';
+  imagenModal5 = '../../../assets/imagenes/galeriaRumania/rum-4-5.jpg';
+  imagenModal6 = '../../../assets/imagenes/galeriaRumania/rum-4-1.jpg';
+  titulo1 = 'Cascada Caprâ';
+  titulo2 = 'Lago Valea';
+  titulo3 = 'Transfăgărășan';
+  // titulo4 = 'Subida al Castillo de Vald Tepes';
+  // titulo5 = 'Subida al Castillo de Vald Tepes';
+  titulo6 = 'Plaza Brasov';
 
   siguiente = 'Transfăgărășan, Făgăraș y Brasov';
   siguienteDonde = '/rumania-dia-4';
