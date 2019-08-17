3208b381fa2ca7acec5fbba6052d70b95c766ff8
MarcosS <sanzgonzalezmarcos@gmail.com>
2019-08-17 11:16:40 +0200

    arregladas fotos

--- src/app/paginas/alemania/berlin/berlin4/berlin4.component.ts
+++ src/app/paginas/alemania/berlin/berlin4/berlin4.component.ts
@@ -8,13 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class Berlin4Component implements OnInit {
 
   imagen1 = '../../../assets/imagenes/galeriaBerlin/IMG_2310.JPG';
-  imagen2 = '../../../assets/imagenes/galeriaBerlin/IMG_2304.JPG';
+  imagen2 = '../../../assets/imagenes/galeriaBerlin/IMG_2305.JPG';
   imagen3 = '../../../assets/imagenes/galeriaBerlin/IMG_2307.JPG';
   imagen4 = '../../../assets/imagenes/galeriaBerlin/IMG_2261.JPG';
   imagen5 = '../../../assets/imagenes/galeriaBerlin/IMG_2317.JPG';
   imagen6 = '../../../assets/imagenes/galeriaBerlin/IMG_2324.JPG';
   imagenModal1 = '../../../assets/imagenes/galeriaBerlin/IMG_2310.JPG';
-  imagenModal2 = '../../../assets/imagenes/galeriaBerlin/IMG_2304.JPG';
+  imagenModal2 = '../../../assets/imagenes/galeriaBerlin/IMG_2305.JPG';
   imagenModal3 = '../../../assets/imagenes/galeriaBerlin/IMG_2307.JPG';
   imagenModal4 = '../../../assets/imagenes/galeriaBerlin/IMG_2261.JPG';
   imagenModal5 = '../../../assets/imagenes/galeriaBerlin/IMG_2317.JPG';
