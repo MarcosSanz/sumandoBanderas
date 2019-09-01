f3e9d951a730c41fa6eb9f68259d8e0d4d374354
MarcosS <sanzgonzalezmarcos@gmail.com>
2019-09-01 21:04:41 +0200

    añadido dia 3 estocolmo

--- src/app/paginas/estocolmo/estocolmo3/estocolmo3.component.ts
+++ src/app/paginas/estocolmo/estocolmo3/estocolmo3.component.ts
@@ -7,6 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Estocolmo3Component implements OnInit {
 
+  imagen1 = '../../../assets/imagenes/galeriaEstocolmo/estocolmodia3-1.JPG';
+  imagen2 = '../../../assets/imagenes/galeriaEstocolmo/IMG-7267.JPG';
+  imagen3 = '../../../assets/imagenes/galeriaEstocolmo/marcosElVikingo.JPG';
+  imagen4 = '../../../assets/imagenes/galeriaEstocolmo/estocolmodia3-5.JPG';
+  imagen5 = '../../../assets/imagenes/galeriaEstocolmo/IMG-7443.JPG';
+  imagen6 = '../../../assets/imagenes/galeriaEstocolmo/estocolmodia3-7.JPG';
+  imagenModal1 = '../../../assets/imagenes/galeriaEstocolmo/estocolmodia3-1.JPG';
+  imagenModal2 = '../../../assets/imagenes/galeriaEstocolmo/IMG-7267.JPG';
+  imagenModal3 = '../../../assets/imagenes/galeriaEstocolmo/marcosElVikingo.JPG';
+  imagenModal4 = '../../../assets/imagenes/galeriaEstocolmo/estocolmodia3-5.JPG';
+  imagenModal5 = '../../../assets/imagenes/galeriaEstocolmo/IMG-7443.JPG';
+  imagenModal6 = '../../../assets/imagenes/galeriaEstocolmo/estocolmodia3-7.JPG';
+  titulo1 = 'Oscarskyrkan';
+  titulo2 = 'Nodisk Museet';
+  titulo3 = 'Marcos Disfrazado de vikingo';
+  titulo4 = 'San Jorge matando al dragón';
+  titulo5 = 'Vistas al Museo Nacional';
+  titulo6 = 'Atardecer en Estocolmo';
+
+  siguiente = 'Palacio de Drottningholm';
+  siguienteDonde = '/estocolmo-dia-4';
+  anterior = 'Birka';
+  anteriorDonde = '/estocolmo-dia-2';
+
   constructor() { }
 
   ngOnInit() {
