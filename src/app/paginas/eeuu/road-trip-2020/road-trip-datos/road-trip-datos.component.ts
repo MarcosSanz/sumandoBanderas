e0aeba7dba0dc57603c3a7441945d8fc220b462e
Marcos Sanz González <msanz@profile.local>
2020-02-04 16:14:30 +0100

    feat: añadido eeuu, falta hacerlo todo

--- src/app/paginas/eeuu/road-trip-2020/road-trip-datos/road-trip-datos.component.ts
+++ src/app/paginas/eeuu/road-trip-2020/road-trip-datos/road-trip-datos.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-road-trip-datos',
   templateUrl: './road-trip-datos.component.html',
   styleUrls: ['./road-trip-datos.component.css']
 })
-export class RoadTripDatosComponent implements OnInit {
+export class RoadTripDatosComponent {
+
+    siguiente = 'Viaje y Bostón';
+    siguienteDonde = '/roadtrip-eeuu-dia-1';
+    anterior = 'Portada del viaje';
+    anteriorDonde = '/roadtrip-eeuu';
 
   constructor() { }
-
-  ngOnInit() {
-  }
-
 }
--- src/app/paginas/eeuu/road-trip-2020/road-trip1/road-trip1.component.ts
+++ src/app/paginas/eeuu/road-trip-2020/road-trip1/road-trip1.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-road-trip1',
-  templateUrl: './road-trip1.component.html',
-  styleUrls: ['./road-trip1.component.css']
+    selector: 'app-road-trip1',
+    templateUrl: './road-trip1.component.html',
+    styleUrls: ['./road-trip1.component.css']
 })
-export class RoadTrip1Component implements OnInit {
+export class RoadTrip1Component {
 
-  constructor() { }
+    siguiente = 'Salem y Bostón';
+    siguienteDonde = '/roadtrip-eeuu-dia-2';
+    anterior = 'Datos del viaje';
+    anteriorDonde = '/roadtrip-eeuu-datos-viaje';
 
-  ngOnInit() {
-  }
+    constructor() { }
 
 }
