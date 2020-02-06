2b2d8f9c54b6b4be3cb4996c17e8d07590ab3690
Marcos Sanz González <msanz@profile.local>
2020-02-06 12:21:24 +0100

    feat: añadidos días de eeuu en progreso

--- src/app/paginas/eeuu/road-trip-2020/road-trip3/road-trip3.component.ts
+++ src/app/paginas/eeuu/road-trip-2020/road-trip3/road-trip3.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-road-trip3',
   templateUrl: './road-trip3.component.html',
   styleUrls: ['./road-trip3.component.css']
 })
-export class RoadTrip3Component implements OnInit {
+export class RoadTrip3Component {
+
+    imagen1 = '../../../assets/imagenes/galeriaPortugal/aveiro-1.jpg';
+    imagen2 = '../../../assets/imagenes/galeriaPortugal/aveiro-2.jpg';
+    imagen3 = '../../../assets/imagenes/galeriaPortugal/aveiro-3.jpg';
+    imagen4 = '../../../assets/imagenes/galeriaPortugal/aveiro-4.jpg';
+    imagen5 = '../../../assets/imagenes/galeriaPortugal/aveiro-5.jpg';
+    imagen6 = '../../../assets/imagenes/galeriaPortugal/batalha-2.jpg';
+    imagen7 = '../../../assets/imagenes/galeriaPortugal/batalha-1.jpg';
+    imagen8 = '../../../assets/imagenes/galeriaPortugal/nazaré-1.jpg';
+    imagen9 = '../../../assets/imagenes/galeriaPortugal/nazare-9.jpg';
+    imagen10 = '../../../assets/imagenes/galeriaPortugal/nazare-3.jpg';
+    imagen11 = '../../../assets/imagenes/galeriaPortugal/nazare-4.JPG';
+    imagen12 = '../../../assets/imagenes/galeriaPortugal/nazare-2.JPG';
+    imagenModal1 = '../../../assets/imagenes/galeriaPortugal/aveiro-1.jpg';
+    imagenModal2 = '../../../assets/imagenes/galeriaPortugal/aveiro-2.jpg';
+    imagenModal3 = '../../../assets/imagenes/galeriaPortugal/aveiro-3.jpg';
+    imagenModal4 = '../../../assets/imagenes/galeriaPortugal/aveiro-4.jpg';
+    imagenModal5 = '../../../assets/imagenes/galeriaPortugal/aveiro-5.jpg';
+    imagenModal6 = '../../../assets/imagenes/galeriaPortugal/batalha-2.jpg';
+    imagenModal7 = '../../../assets/imagenes/galeriaPortugal/batalha-1.jpg';
+    imagenModal8 = '../../../assets/imagenes/galeriaPortugal/nazaré-1.jpg';
+    imagenModal9 = '../../../assets/imagenes/galeriaPortugal/nazare-9.jpg';
+    imagenModal10 = '../../../assets/imagenes/galeriaPortugal/nazare-3.jpg';
+    imagenModal11 = '../../../assets/imagenes/galeriaPortugal/nazare-4.JPG';
+    imagenModal12 = '../../../assets/imagenes/galeriaPortugal/nazare-2.JPG';
+    titulo1 = 'Aveiro';
+    titulo2 = 'Sé de Aveiro';
+    titulo3 = 'Pintorescas casas de las playas de Aveiro';
+    titulo4 = 'Pintorescas casas de las playas de Aveiro';
+    titulo5 = 'Faro de Aveiro';
+    titulo6 = 'Monasterio de Batalha';
+    titulo7 = 'Monasterio de Batalha';
+    titulo8 = 'Nazaré';
+    titulo9 = 'Vistas del Atlántico';
+    titulo10 = 'Faro de Nazaré';
+    titulo11 = 'Vistas del Atlántico';
+    titulo12 = 'Nazaré';
+
+    siguiente = 'Philadelphia, Lancaster y Gettysburg';
+    siguienteDonde = '/roadtrip-eeuu-dia-4';
+    anterior = 'Salem y Bostón';
+    anteriorDonde = '/roadtrip-eeuu-dia-2';
 
   constructor() { }
-
-  ngOnInit() {
-  }
-
 }
