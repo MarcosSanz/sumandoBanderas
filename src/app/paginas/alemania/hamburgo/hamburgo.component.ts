4c8c7b0c06c40591f049b754f2f25ed0a66c365f
Marcos Sanz González <msanz@profile.local>
2020-02-27 15:46:18 +0100

    feat: añadidas fotos de hamburgo, cuenca y tavira

--- src/app/paginas/alemania/hamburgo/hamburgo.component.ts
+++ src/app/paginas/alemania/hamburgo/hamburgo.component.ts
@@ -9,30 +9,30 @@ export class HamburgoComponent {
 
     // Carrusel de la portada
     tituloCarrusel1 = 'Boston';
-    imagenCarrusel1 = '../../../assets/imagenes/galeriaEEUU/dia2-17.jpg';
+    imagenCarrusel1 = '../../../assets/imagenes/galeriaHamburgo/hamburgo1.jpg';
     descripcion1 = 'Massachusetts State House';
     tituloCarrusel2 = 'Philadelphia';
-    imagenCarrusel2 = '../../../assets/imagenes/galeriaEEUU/dia3-6.jpg';
+    imagenCarrusel2 = '../../../assets/imagenes/galeriaHamburgo/hamburgo3.jpg';
     descripcion2 = 'Parque Love';
     tituloCarrusel3 = 'Washington D.C.';
-    imagenCarrusel3 = '../../../assets/imagenes/galeriaEEUU/dia6-5.jpg';
+    imagenCarrusel3 = '../../../assets/imagenes/galeriaHamburgo/dia6-5.jpg';
     descripcion3 = 'Capitolio';
     tituloCarrusel4 = 'Nueva York';
-    imagenCarrusel4 = '../../../assets/imagenes/galeriaEEUU/dia7-3.jpg';
+    imagenCarrusel4 = '../../../assets/imagenes/galeriaHamburgo/dia7-3.jpg';
     tituloCarrusel5 = 'Brooklyn';
-    imagenCarrusel5 = '../../../assets/imagenes/galeriaEEUU/dia9-5.jpg';
+    imagenCarrusel5 = '../../../assets/imagenes/galeriaHamburgo/dia9-5.jpg';
 
     // Tarjetas de cada dia
     tituloDia1 = 'Día 1 - Hamburgo';
-    imagenDia1 = '../../../assets/imagenes/galeriaEEUU/dia6-2.jpg';
+    imagenDia1 = '../../../assets/imagenes/galeriaHamburgo/hamburgo2.jpg';
     enlaceDia1 = '/hamburgo';
 
     tituloDia2 = 'Día 2 - Stade y Neuengamme';
-    imagenDia2 = '../../../assets/imagenes/galeriaEEUU/dia1-1.jpg';
+    imagenDia2 = '../../../assets/imagenes/galeriaHamburgo/dia1-1.jpg';
     enlaceDia2 = '/stade-y-neuengamme';
 
     //   tituloDia3 = 'Día 3 - Bremen';
-    //   imagenDia3 = '../../../assets/imagenes/galeriaEEUU/dia2-1.jpg';
+    //   imagenDia3 = '../../../assets/imagenes/galeriaHamburgo/dia2-1.jpg';
     //   enlaceDia3 = '/bremen';
 
     constructor() { }
