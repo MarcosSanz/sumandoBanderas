77405bacb341c85437e824d8ebd6cd58892a3a10
MarcosS <sanzgonzalezmarcos@gmail.com>
2020-09-23 09:59:02 +0200

    feat: palencia

--- src/app/paginas/castilla/aguilar/aguilar.component.ts
+++ src/app/paginas/castilla/aguilar/aguilar.component.ts
@@ -7,25 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AguilarComponent {
 
-    imagen1 = '../../../assets/imagenes/galeriaAsturias/oviedo3.jpg';
-    imagen2 = '../../../assets/imagenes/galeriaAsturias/oviedo6.jpg';
-    imagen3 = '../../../assets/imagenes/galeriaAsturias/oviedo5.jpg';
-    imagen4 = '../../../assets/imagenes/galeriaAsturias/oviedo4.jpg';
-    imagen5 = '../../../assets/imagenes/galeriaAsturias/oviedo7.jpg';
-    imagen6 = '../../../assets/imagenes/galeriaAsturias/oviedo8.jpg';
-    imagenModal1 = '../../../assets/imagenes/galeriaAsturias/oviedo3.jpg';
-    imagenModal2 = '../../../assets/imagenes/galeriaAsturias/oviedo6.jpg';
-    imagenModal3 = '../../../assets/imagenes/galeriaAsturias/oviedo5.jpg';
-    imagenModal4 = '../../../assets/imagenes/galeriaAsturias/oviedo4.jpg';
-    imagenModal5 = '../../../assets/imagenes/galeriaAsturias/oviedo7.jpg';
-    imagenModal6 = '../../../assets/imagenes/galeriaAsturias/oviedo8.jpg';
-    titulo1 = 'Santa María del Naranco';
-    titulo2 = 'San Miguel de Lillo';
-    titulo3 = 'Mafalda';
-    titulo4 = 'Catedral de Oviedo';
-    titulo5 = 'El viajero';
-    titulo6 = 'Plaza del Ayuntamiento';
-
     siguiente = 'Castilla';
     siguienteDonde = '/castilla';
     anterior = 'Cuenca';
